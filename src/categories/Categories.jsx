684d19f676c95cba63333648ca2fbc9bf1d665c8
ggquicky <ggquicky@gmail.com>
2022-08-30 23:22:17 -0600

    fix: changing from npm to pnpm

--- src/categories/Categories.jsx
+++ src/categories/Categories.jsx
@@ -1,14 +1,14 @@
 import Items from "./Items";
 
 export default function Categories() {
-  Allcategories = ["All", "Vegetables", "Fruits", "Coffe & teas", "Meat"];
+  const Allcategories = ["All", "Vegetables", "Fruits", "Coffe & teas", "Meat"];
 
   return (
     <div>
       <div>
         <span>Explore Categories</span>
         <div>
-          {this.Allcategories.map(function (element, index) {
+          {Allcategories.map(function (element, index) {
             return (
               <a key={index} href="/">
                 {element}
--- src/nav/navBar.jsx
+++ src/nav/navBar.jsx
@@ -1,7 +1,6 @@
 import { Menuitems } from "./MenuItems";
 
 export default function Navbar() {
-  console.log(Menuitems);
   return (
     <nav>
       {Menuitems.map((item, index) => {
